4efec27e2029305d1d5a863100b23def0b414005
Megha <meghaprasadkeecheril@mail.com>
2023-04-24 18:52:13 +0530

    incmplt

--- script/script.js
+++ script/script.js
@@ -6,8 +6,38 @@ hamburger.addEventListener("click", function ()
  menu.classList.toggle("hidden");
  }); 
 window.addEventListener("resize", function () { 
-    if (window.innerWidth > 970) { 
+    if (window.innerWidth > 910) { 
         hamburger.classList.add("hidden");
          menu.classList.add("hidden");
          } 
         });
+
+
+
+        // $('.carousel').slick({
+        //     centerMode: true,
+        //     centerPadding: '60px',
+        //     slidesToShow: 4,
+        //     autoplaySpeed: 2000,
+        //     autoplay: false,
+        //     responsive: [
+        //       {
+        //         breakpoint: 768,
+        //         settings: {
+        //           arrows: false,
+        //           centerMode: true,
+        //           centerPadding: '40px',
+        //           slidesToShow: 3
+        //         }
+        //       },
+        //       {
+        //         breakpoint: 480,
+        //         settings: {
+        //           arrows: false,
+        //           centerMode: true,
+        //           centerPadding: '40px',
+        //           slidesToShow: 1
+        //         }
+        //       }
+        //     ]
+        //   });
